eb0582e44371870b5f23823ce705576f1fb509a8
mohitkalal1 <mohitkalal007mk@gmail.com>
2022-10-12 19:05:06 +0530

    Playlist Added

---
+++ spotify-frontend-master/spotify-frontend-master/src/components/ViewAllPlaylist.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+
+
+export const ViewAllPlaylist = () => {
+
+    const [playlists, setplaylists] = useState([])
+    const fetchPlaylist = async () => {
+
+        axios.get("http://localhost:8000/playlist/playlistdata").then((res) => {
+            setplaylists(res.data);
+        });
+    };
+
+    const removePlayList = async (id) => {
+        axios.delete(`http://localhost:8000/playlist/playlist/${id}`).then((res) => {
+            console.log(res);
+            fetchPlaylist();
+        });
+    }
+
+    const removesongFromPlaylist = async (playlistid,id) => {
+        
+        var songs ={
+            songId:id
+        }
+        console.log(id);
+        axios.put(`http://localhost:8000/playlist/playlist1/${playlistid}`,songs
+        ).then((res) => {
+            console.log(res);
+            fetchPlaylist();
+        });
+    }
+
+    useEffect(() => {
+      
+    
+      fetchPlaylist();
+    }, [])
+    
+  return (
+    <div>
+          <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th> 
+          </tr>
+        </thead>
+        <tbody>
+          {
+                playlists.map((playlist,index)=>{   
+                    return(
+                        <tr>
+                            
+                            <th scope="row">{index+1}</th>
+                            
+                            <td>{playlist.name}</td>
+                            <td>{playlist.artist}</td>
+                            <td>
+                                <button className="btn btn-danger" onClick={()=>{removePlayList(playlist._id)}}>DELETE PLAYL-LIST</button>
+                            </td>
+                            <td>
+                            <ul>
+                                {
+                                    playlist.songs.map((song)=>{
+                                        return(
+                                            <>
+                                            <li>{song.name}</li>
+                                            <li>
+                                                <button onClick={()=>{
+                                                    removesongFromPlaylist(playlist._id,song._id)
+                                                }} class="btn btn-danger">Remove song</button>
+                                            </li>
+                                            </>
+                                        )
+                                    })
+                                }
+                            </ul>
+                            </td>
+                        </tr>
+                    )
+                })
+          }
+          
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
